8f726e0ed956be0af5571f3a867e6da8756ed40c
Charles Hamilton <HudsonWerks@users.noreply.github.com>
2016-05-04 17:23:19 -0400

    Create ground_control_mac.asm

---
+++ ground_control_mac.asm
@@ -0,0 +1,15 @@
+global start
+start:
+        mov rdi, 1              ; write to standard out = 1
+        mov rsi, message        ; Put the offset of message in ecx
+        mov rdx, strlen         ; the size to write
+        mov rax, 0x2000004      ; The system call for write = 4 (sys_write)
+        syscall                 ; invoke the kernel
+        mov rax, 0x2000001      ; system call number for exit = 1 (sys_exit)
+        mov rdi, 0              ; Exit with return code of 0 (no error)
+        syscall                 ; invoke the kernel
+
+section .data
+message:
+        db 'Ground Control to Major Tom', 0x0a    ; 'Your message' plus a linefeed character
+strlen equ $ - message
